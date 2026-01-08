e4d29927baf0d9a48b5353cf8e10413c4e307d8b
Luis Porras <lporras16@gmail.com>
2026-01-08 12:28:11 -0500

    fix icons

--- app/javascript/controllers/playlist_chords_controller.js
+++ app/javascript/controllers/playlist_chords_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["toggleButton", "buttonText", "content", "chords"]
+  static targets = ["toggleButton", "buttonText", "iconChords", "iconLyrics", "content", "chords"]
 
   connect() {
     this.showingChords = false
@@ -15,11 +15,27 @@ export default class extends Controller {
       this.contentTargets.forEach(el => el.classList.add('hidden'))
       this.chordsTargets.forEach(el => el.classList.remove('hidden'))
       this.buttonTextTarget.textContent = 'Ver Letras'
+
+      // Update button styling to show active state
+      this.toggleButtonTarget.classList.remove('btn-primary')
+      this.toggleButtonTarget.classList.add('btn-accent')
+
+      // Swap icons: hide music note, show document
+      this.iconChordsTarget.classList.add('hidden')
+      this.iconLyricsTarget.classList.remove('hidden')
     } else {
       // Show content, hide chords
       this.contentTargets.forEach(el => el.classList.remove('hidden'))
       this.chordsTargets.forEach(el => el.classList.add('hidden'))
       this.buttonTextTarget.textContent = 'Ver Acordes'
+
+      // Reset button styling to default state
+      this.toggleButtonTarget.classList.remove('btn-accent')
+      this.toggleButtonTarget.classList.add('btn-primary')
+
+      // Swap icons: show music note, hide document
+      this.iconChordsTarget.classList.remove('hidden')
+      this.iconLyricsTarget.classList.add('hidden')
     }
   }
 }
